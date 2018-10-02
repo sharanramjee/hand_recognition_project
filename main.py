ac76dea4627be71c945ce84894df04a8c75b7770
Brandon Stewart <stewartb2@gmail.com>
2018-10-02 07:44:29 -0400

    cleanup code

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import cv2
 import time
 
-#On my laptop
-#0 front camera
-#1 back camera
-cap = cv2.VideoCapture(1)
 
-for x in range(10):
-    ret, frame = cap.read()
-    cv2.imshow('frame', frame)
-    #out = cv2.imwrite('./capture' + str(x) + '.jpg', frame)
-    cv2.waitKey(1)
-    time.sleep(3)
-    
+def get_images():
 
-cap.release()
+    # On my laptop
+    # 0 front camera
+    # 1 back camera
+    camera = 0
+    cap = cv2.VideoCapture(camera)
+
+    # Capture X number of images
+    for x in range(10):
+        # take image
+        ret, frame = cap.read()
+
+        # display image
+        cv2.imshow('frame', frame)
+
+        # write image to file
+        #out = cv2.imwrite('./capture' + str(x) + '.jpg', frame)
+        cv2.waitKey(1)
+
+        time.sleep(3)
+
+    cap.release()
+
+
+get_images()
