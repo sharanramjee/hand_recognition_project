560a70948a301047b08fdfdce463915e0b2f26eb
Brandon Stewart <stewartb2@gmail.com>
2018-10-01 20:15:57 -0400

    take picture every 3 seconds

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import time
 #On my laptop
 #0 front camera
 #1 back camera
-cap = cv2.VideoCapture(0)
-ret, frame = cap.read()
+cap = cv2.VideoCapture(1)
 
 for x in range(10):
+    ret, frame = cap.read()
     cv2.imshow('frame', frame)
-    out = cv2.imwrite('./capture' + str(x) + '.jpg', frame)
+    #out = cv2.imwrite('./capture' + str(x) + '.jpg', frame)
     cv2.waitKey(1)
     time.sleep(3)
     
