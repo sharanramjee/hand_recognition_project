19e5c266c1960958a68bbb8a8fda68ab95e5b3c3
Brandon Stewart <stewartb2@gmail.com>
2018-10-10 20:28:56 -0400

    Fix macOS specific issues

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import cv2
 import time
+import platform
 
+IS_MAC = (platform.system() == 'Darwin')
 
-def get_images():
+def get_images(numSamples: int, secsBetweenSamples: float):
 
     # On my laptop
     # 0 front camera
@@ -11,20 +13,28 @@ def get_images():
     cap = cv2.VideoCapture(camera)
 
     # Capture X number of images
-    for x in range(10):
+    for x in range(numSamples):
         # take image
         ret, frame = cap.read()
 
         # display image
         cv2.imshow('frame', frame)
 
+        #   Mac default camera is 1280 by 720 (16:9)
+        if (IS_MAC):
+            frame = frame[:, 160:1120, :]   #   Extract the middle column of the image (go from 16:9 resolution to 4:3 resolution)
+
+        #   Ensure common resolution
+        frame = cv2.resize(frame, (640, 480))
+
         # write image to file
-        #out = cv2.imwrite('./capture' + str(x) + '.jpg', frame)
+        if (not IS_MAC or x):   #   The first image doesn't save on mac
+            out = cv2.imwrite('./capture{}.jpg'.format(x), frame)
         cv2.waitKey(1)
 
-        time.sleep(3)
+        time.sleep(secsBetweenSamples)
 
     cap.release()
 
-
-get_images()
+if __name__ == '__main__':
+    get_images(10, 1)
